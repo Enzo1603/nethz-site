c6edd5a70f2b1cc5a563b7fbb7eb8166ae90a27b
Enzo Baraldi <96661578+Enzo1603@users.noreply.github.com>
2023-02-15 19:11:36 +0100

    First commit, basic page layout

---
+++ app.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from werkzeug.exceptions import HTTPException
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.context_processor
+def inject_now():
+    return {"utcnow": datetime.utcnow()}
+
+
+@app.errorhandler(HTTPException)
+def custom_error_page(e):
+    return render_template("main/errors.html", error_message=e), e.code
+
+
+@app.route("/")
+def home():
+    return render_template("main/home.html")
+
+
+@app.route("/technische_mechanik/<string:semester>")
+def technische_mechanik(semester: str):
+    return render_template(f"technische_mechanik/TM_{semester}.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
