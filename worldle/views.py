0b308e393d80956383a9e41439f39058c16aa018
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2023-11-19 20:49:21 +0100

    Reworked worldle capital to be more resilient

--- worldle/views.py
+++ worldle/views.py
@@ -4,38 +4,40 @@ from pathlib import Path
 
 from worldle import worldle_bp
 
-from flask import render_template, request
+from flask import abort, render_template
 
 FILE_PATH = Path(__file__).resolve().parent
-
 
 
 @worldle_bp.route("/")
 def home():
     return render_template("worldle/home.html")
 
-@worldle_bp.route("/capital")
-def capital():
-    valid_regions = {
-        "Africa", "Americas", "Antarctic", "Asia", "Europe", "Oceania", "Worldwide"
+
+@worldle_bp.route("/capital/<string:region>")
+def capital(region="worldwide"):
+    VALID_REGIONS = {
+        "africa", "americas", "antarctic", "asia", "europe", "oceania", "worldwide"
     }
-    region = request.args.get("region")
 
-    if region not in valid_regions:
-        region = "Worldwide" # Default region
-
+    if region not in VALID_REGIONS:
+        abort(404)
 
     countries_csv_file_path = FILE_PATH.parent / "static" / "worldle" / "countries.csv"
     with open(countries_csv_file_path) as f:
         reader = csv.DictReader(f)
         entries = list(reader)
 
-        if region != "Worldwide":
-            entries = [entry for entry in entries if entry["region"].lower() == region.lower()]
+        if region != "worldwide":
+            entries = [
+                entry for entry in entries if entry["region"].lower() == region.lower()
+            ]
 
         # Filter entries with no capitals
         entries = [entry for entry in entries if entry["capital"].strip()]
 
         random_row = random.choice(entries)
 
-    return render_template("worldle/capital.html", country_data=random_row)
+    return render_template(
+        "worldle/capital.html", region=region, country_data=random_row
+    )
